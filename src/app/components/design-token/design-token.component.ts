f5863f282261c881223400b4617000911554bf6c
Alex Vong <avong2011@gmail.com>
2021-11-03 08:02:10 -0400

    feat: optimizing token animations

--- src/app/components/design-token/design-token.component.ts
+++ src/app/components/design-token/design-token.component.ts
@@ -6,8 +6,10 @@ import {
   HostBinding,
   NgZone,
   OnInit,
+  QueryList,
   Renderer2,
   ViewChild,
+  ViewChildren,
 } from '@angular/core';
 import {gsap} from 'gsap';
 
@@ -23,18 +25,21 @@ export class DesignTokenComponent implements OnInit, AfterViewInit {
   @ViewChild('tokenRing2') tokenRing2!: ElementRef;
   @ViewChild('tokenRing3') tokenRing3!: ElementRef;
   @ViewChild('tokenRing4') tokenRing4!: ElementRef;
+  @ViewChild('tokenSheen') tokenSheen!: ElementRef;
+  @ViewChildren('tokenCubeStroke', {read: ElementRef}) tokenCubeStroke!: QueryList<ElementRef>;
   constructor(private element: ElementRef, private render: Renderer2, private ngZone: NgZone) {}
 
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
+    const tokenCubeStrokes = this.tokenCubeStroke.map((stroke) => stroke.nativeElement);
     const token = gsap.timeline({
       defaults: {
-        ease: 'bounce',
-        duration: 1.75,
+        ease: 'back',
+        duration: 3,
         repeat: -1,
+        repeatDelay: 3,
         yoyo: true,
-        //stagger: 0.125
       },
     });
 
@@ -60,7 +65,7 @@ export class DesignTokenComponent implements OnInit, AfterViewInit {
           strokeWidth: 3,
           opacity: 1,
         },
-        0.25
+        0.2
       )
       .fromTo(
         this.tokenRing3.nativeElement,
@@ -72,7 +77,7 @@ export class DesignTokenComponent implements OnInit, AfterViewInit {
           strokeWidth: 6,
           opacity: 1,
         },
-        0.45
+        0.4
       )
       .fromTo(
         this.tokenRing4.nativeElement,
@@ -84,7 +89,36 @@ export class DesignTokenComponent implements OnInit, AfterViewInit {
           strokeWidth: 3,
           opacity: 1,
         },
-        0.75
+        0.6
+      )
+      .fromTo(
+        tokenCubeStrokes,
+        {
+          strokeDasharray: 400,
+          strokeDashoffset: 400,
+          opacity: 0,
+        },
+        {
+          strokeDasharray: 0,
+          strokeDashoffset: 0,
+          opacity: 1,
+          stagger: 0.175,
+        },
+        1.2
+      )
+      .fromTo(
+        this.tokenSheen.nativeElement,
+        {
+          rotate: -25,
+          opacity: 0,
+          transformOrigin: 'center',
+        },
+        {
+          rotate: 0,
+          opacity: 1,
+          transformOrigin: 'center',
+        },
+        1.2
       );
   }
 }
