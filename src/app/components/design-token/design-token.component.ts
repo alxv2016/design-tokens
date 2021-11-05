e6dad360491166c4f784ff291942d27703ddbf78
Alex Vong <avong2011@gmail.com>
2021-11-04 20:06:03 -0400

    feat: updated token animations

--- src/app/components/design-token/design-token.component.ts
+++ src/app/components/design-token/design-token.component.ts
@@ -24,40 +24,62 @@ export class DesignTokenComponent implements OnInit, AfterViewInit {
   @ViewChild('tokenSheen') tokenSheen!: ElementRef;
   @ViewChildren('tokenRing', {read: ElementRef}) tokenRing!: QueryList<ElementRef>;
   @ViewChildren('tokenCubeStroke', {read: ElementRef}) tokenCubeStroke!: QueryList<ElementRef>;
+  @ViewChildren('tokenGlitch', {read: ElementRef}) tokenGlitch!: QueryList<ElementRef>;
+  @ViewChildren('lensFlare', {read: ElementRef}) lensFlare!: QueryList<ElementRef>;
   constructor(private element: ElementRef, private render: Renderer2, private ngZone: NgZone) {}
 
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
+    const tokenGlitches = this.tokenGlitch.map((glitch) => glitch.nativeElement);
     const tokenRings = this.tokenRing.map((ring) => ring.nativeElement);
     const tokenCubeStrokes = this.tokenCubeStroke.map((stroke) => stroke.nativeElement);
+    const lensFlares = this.lensFlare.map((flare) => flare.nativeElement);
+
     const token = gsap.timeline({
+      repeat: -1,
+      repeatDelay: 3,
+      yoyo: true,
+      yoyoEase: 'back',
       defaults: {
         ease: 'back',
-        duration: 3,
-        repeat: -1,
-        repeatDelay: 2,
-        yoyo: true,
+        duration: 2.25,
       },
     });
 
     token
+      .from(tokenGlitches, {
+        opacity: 0,
+        scale: 0.95,
+        stagger: 0.275,
+        transformOrigin: 'center',
+      })
       .from(tokenRings, {
         strokeWidth: 1.25,
         opacity: 0,
         stagger: 0.275,
         ease: 'bounce',
       })
+      .from(
+        tokenCubeStrokes,
+        {
+          strokeDasharray: 400,
+          strokeDashoffset: 400,
+          opacity: 0,
+          stagger: 0.275,
+        },
+        2.75
+      )
+      .from(lensFlares, {
+        ease: 'power3',
+        scale: 0.125,
+        opacity: 0,
+        transformOrigin: 'center',
+      })
       .from(this.tokenSheen.nativeElement, {
         rotate: -25,
         opacity: 0,
         transformOrigin: 'center',
-      })
-      .from(tokenCubeStrokes, {
-        strokeDasharray: 400,
-        strokeDashoffset: 400,
-        opacity: 0,
-        stagger: 0.275,
       });
   }
 }
